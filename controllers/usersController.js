c0d546fe65e7ee094c8a53f36a710b5d73490434
Thorjuri <sjl1992@naver.com>
2022-12-26 20:11:40 +0900

    12/26 friends 전체 api 기초 구현 완료

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,24 +6,25 @@ class UsersController {
     signup = async(req, res)=> {
         const { loginId, password } = req.body;
         const data = await this.usersService.signup(loginId, password);
-        res.send(data);
+        res.status(201).send(data);
     };
 
     checkId = async(req, res)=> {
         const { loginId } = req.body;
         const data = await this.usersService.checkId(loginId);
-        res.send(data);
+        res.status(200).send(data);
     };
 
     login = async(req, res)=> {
         const { loginId, password } = req.body;
         const data = await this.usersService.login(loginId, password);
-        res.send(data);
+        res.status(200).send(data);
     };
 
     getAllUsers = async(req, res)=> {
-        const data = await this.usersService.getAllUsers();
-        res.send(data);
+        const { loginId } = res.locals.user
+        const data = await this.usersService.getAllUsers(loginId);
+        res.status(200).send(data);
     };
 };
 
--- routes/friends.js
+++ routes/friends.js
@@ -5,12 +5,12 @@ const FriendsController = require("../controllers/friendsController");
 const friendsController = new FriendsController();
 const wrapAsyncController = require('../middlewares/wrapAsyncController');
 
-router.post('/', auth_middleware, wrapAsyncController(friendsController.newRequest));
-router.get('/', auth_middleware, wrapAsyncController(friendsController.getRequest));
+router.post('/', auth_middleware, wrapAsyncController(friendsController.createReq));
+router.get('/', auth_middleware, wrapAsyncController(friendsController.getNewReq));
 router.post('/add', auth_middleware, wrapAsyncController(friendsController.acceptReq));
 router.delete('/add', auth_middleware, wrapAsyncController(friendsController.rejectReq));
 router.get('/me', auth_middleware, wrapAsyncController(friendsController.getMyFriend));
-router.delete('/me', auth_middleware, wrapAsyncController(friendsController.dropFriend))
+router.patch('/me', auth_middleware, wrapAsyncController(friendsController.dropFriend))
 
 
 module.exports = router;
