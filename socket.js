6e450a50ced7dc29d2ecb7a929b80d7ef7d9446a
Thorjuri <sjl1992@naver.com>
2022-12-28 12:06:25 +0900

    12/28 view 프론트 통신 에러 해결 - Users조회, Friends 친구요청

--- socket.js
+++ socket.js
@@ -55,30 +55,24 @@ module.exports = (http) => {
     io.on("connection", (socket) => {
         const userNum1 = countRoom("1");
         const userNum2 = countRoom("2");
-        // console.log(userNum1, userNum2)
         socket.emit("list", (userNum1, userNum2));
 
-        socket.on("connection", (obj)=> {
+        socket.on("enterRoom", (obj)=> {
             const convert = JSON.parse(obj);
             const { roomNum, nickname } = convert;
-            console.log("---------roomNumber---------",roomNum );
             socket.join(roomNum);
             const message = `${nickname}님이 ${roomNum}방에 입장하셨습니다.`;
             const userNum = countRoom(roomNum);
             io.sockets.in(roomNum).emit("welcome", message, roomNum, userNum);
-            // console.log("---1---", roomNum )
         });
 
         socket.on("chat", (obj)=> {
             const convert = JSON.parse(obj);
             const { roomNum , nickname, message } = convert;
             io.sockets.in(roomNum).emit("contents", roomNum, nickname, message);
-            // console.log("---2---", roomNum , nickname, message )
-            // console.log("---현재 접속자---", findUser())
         });
 
         socket.on("disconnecting", (nickname) => {
-            // console.log("---퇴장 후 잔여 접속자---", findUser())
             const message = `${nickname} 님이 퇴장하셨습니다.`;
             socket.emit("chat", message); 
         });
